c9ef540ec0d4cfbdeab17486e68c6684137e3b2a
enrique723 <glianjose@gmail.com>
2024-10-29 20:33:47 -0600

    3.2

---
+++ Vistas/Home.py
@@ -0,0 +1,35 @@
+import json
+import requests
+import streamlit as st 
+from streamlit_lottie import st_lottie
+
+def get (path:str):
+    with open (path, "r") as p:
+        return json.load(p)
+    
+def get_url (url:str):
+    r=requests.get(url)
+    if r.status_code !=200:
+        return None
+    return r.json()
+
+with st.container():
+    st.subheader("Bienvenido, somos SoftIA")
+    st.title("Creamos soluciones para acelrar tu negocio")
+    st.write("Somos apacionados de las tecnologias y la innovacion")
+    st.write("[Saber mas>](https://streamlit.io/)")
+    
+    with st.container():
+        st.write("---")
+        l_columna, r_columna = st.columns((2))
+        with l_columna:
+            st.header("Sobre nosotros")
+            st.write("asdasdasdadasdadasdasdasdasdjskdblksahvkdjbvflkajsdbvlasjhdbcljsdbclkasdhbclsahbdclasjdkbclasdbclashdbclajshcdvljashdcjlashdbcljasdhbclashdbclsajdch sa")
+            st.write("[Saber mas>](https://streamlit.io/)")
+
+        with r_columna:
+            path = get ("./animation/ani.json")
+            st_lottie(path)
+            
+            url = get_url("https://lottie.host/8611e424-5454-46ef-acc1-dbe8a675c7ed/GenBO7VdlL.json")
+            st_lottie(url)
