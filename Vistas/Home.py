b90a5adbb128089a1701c2c8912425fb17757365
enrique723 <glianjose@gmail.com>
2024-10-30 20:16:56 -0600

    correcciones

--- Vistas/Home.py
+++ Vistas/Home.py
@@ -1,30 +1,65 @@
-import json
-import requests
-import streamlit as st 
-from streamlit_lottie import st_lottie
+import streamlit as  st
 
-def get (path:str):
-    with open (path, "r") as p:
-        return json.load(p)
+#Inicio de pagina
+with st.container():
+    st.subheader("Bienvenidos, Somos softia")
+    st.title("Creamos soluciones para acelerar tu negocio")
+    st.write(
+        "Somos unos apasionados de las tenologías y la innovación, especialización en el sector de la digitalización y automatización de negocios. Nos gusta crear soluciones para resolver problemas y mejorar procesos. "
+    )
+    st.write("[Saber más>]((https://share.streamlit.io/user/diether28))"
+    )
+
+#Sobre nosostros
+with st.container():
+    st.write("---")
+
+    texto_columna, imagen_columna = st.columns((1,2))
+    with texto_columna:
+        st.subheader("Sobre nosotros")
+        st.write(
+            """
+            Nuestro objetivo es poder aportar valor a los negocios ayudandoles a ahorrar tiempo y dinero a través de la implantación de nuevas tecnologías como la inteligencia artifical, analisis de datos o implantación de software de automatización.
+
+Seguramente te vamos a poder ayudar si:
+
+-Tienes un negocio y quieres mejorar tus procesos de trabajo para ahorrar tiempo y dinero Tienes trabajadores que emplean parte de su jornada a realizar tareas repetitivas sin valor añadido
+
+para tu negocio
+
+No tienes claras las métricas de tu negocio y quieres tomar decisiones basadas en datos
+
+-Quieres mejorar la experiencia de tus clientes
+
+Usas herramientas de software antiguas o poco eficientes o procesos en los que usas papel y boligrafo
+
+***Si esto suena interesante para ti puedes contactarnos a través del formulario que encontrarás al final de la página***
+
+            """
+    )
+    with imagen_columna:
+        st.image("Img/robot.png")
     
-def get_url (url:str):
-    r=requests.get(url)
-    if r.status_code !=200:
-        return None
-    return r.json()
+
+#Servicios
 
 with st.container():
-    st.subheader("Bienvenido, somos SoftIA")
-    st.title("Creamos soluciones para acelrar tu negocio")
-    st.write("Somos apacionados de las tecnologias y la innovacion")
-    st.write("[Saber mas>](https://streamlit.io/)")
-    
-    with st.container():
-        st.write("---")
-        l_columna, r_columna = st.columns((2))
-        with l_columna:
-            st.header("Sobre nosotros")
-            st.write("asdasdasdadasdadasdasdasdasdjskdblksahvkdjbvflkajsdbvlasjhdbcljsdbclkasdhbclsahbdclasjdkbclasdbclashdbclajshcdvljashdcjlashdbcljasdhbclashdbclsajdch sa")
-            st.write("[Saber mas>](https://streamlit.io/)")
+    st.write("---")
+    st.header("Servicios")
 
-      
+
+    texto_columna, imagen_columna = st.columns((1,2))
+
+
+with texto_columna:
+    st.subheader("Diseño de aplicaciones")
+
+    st.write(
+        """
+        Si en tus procesos diarios tienes que introducir información en diferentes fuentes de datos o bien tienes que 
+        trabajar con documentación en papel, es hora de pensar en implementar una aplicación en tu negocio para potenciar y optimizar el funcionamiento de los procesos diarias
+        """
+    )
+    st.write("[Ver servicios >](https://share.streamlit.io/user/diether28)")
+    
+    with imagen_columna: st.image("Img/robot.png")
